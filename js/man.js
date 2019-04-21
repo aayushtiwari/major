6798f4b5395bdc0272993612dfa15103e3d2076d
Aayush in Beta <arsenicaayush@gmail.com>
2019-04-21 21:48:55 +0530

    Add files via upload

--- js/man.js
+++ js/man.js
@@ -2,5 +2,4 @@ function clickedButton() {
   localStorage.removeItem("search");
   localStorage.setItem("search", $("input[name=sea]").val());
   location.href = "https://aayushtiwari.github.io/major/list";
-
 }
