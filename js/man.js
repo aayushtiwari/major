835ec06bf60f248b22a30056308f250b3d380cd8
Aayush in Beta <arsenicaayush@gmail.com>
2019-04-21 21:38:32 +0530

    Update man.js

--- js/man.js
+++ js/man.js
@@ -1,3 +1,5 @@
 function clickedButton() {
   location.href = "https://aayushtiwari.github.io/major/list";
+  localStorage.removeItem("search");
+  localStorage.setItem("search", $("input[name=sea]").val());
 }
